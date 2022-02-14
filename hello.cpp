41700e3e93baee8a55de3f73895a359ce64cdf92
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2022-02-14 11:32:02 +0000

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void helloFromHelloCpp() {
+	std::cout << "Hello world from hello.cpp" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void helloFromHelloCpp();
+
+int main() {
+	std::cout << "Hello world from main.cpp" << std::endl;
+	helloFromHelloCpp();
+}
